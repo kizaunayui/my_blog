d66cc27f10d44a722424a2005cf0d83881ec876c
kizaunayui <112533480+kizaunayui@users.noreply.github.com>
2026-05-18 20:07:14 +0800

    Type nested navigation links

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,6 +1,15 @@
 import { contentSections } from './contentSections'
 
-const headerNavLinks = [
+type HeaderNavChild = {
+  href: string
+  title: string
+}
+
+type HeaderNavLink = HeaderNavChild & {
+  children?: readonly HeaderNavChild[]
+}
+
+const headerNavLinks: HeaderNavLink[] = [
   { href: '/', title: '首页' },
   { href: '/articles', title: '文章' },
   { href: '/content', title: '内容中心', children: contentSections },
