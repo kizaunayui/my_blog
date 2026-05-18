30d843877cf3b54f118ae7c0db22a03142a49caf
kizaunayui <112533480+kizaunayui@users.noreply.github.com>
2026-05-18 20:09:05 +0800

    Add static params to old blog pagination redirect

--- app/blog/page/[page]/page.tsx
+++ app/blog/page/[page]/page.tsx
@@ -1,4 +1,12 @@
+import { allBlogs } from 'contentlayer/generated'
 import { redirect } from 'next/navigation'
+
+const POSTS_PER_PAGE = 5
+
+export const generateStaticParams = async () => {
+  const totalPages = Math.ceil(allBlogs.length / POSTS_PER_PAGE)
+  return Array.from({ length: totalPages }, (_, index) => ({ page: (index + 1).toString() }))
+}
 
 export default async function Page(props: { params: Promise<{ page: string }> }) {
   const params = await props.params
