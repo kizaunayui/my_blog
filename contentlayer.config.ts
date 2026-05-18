5826df5540083ea3d8c2edc8b61b349838f3cdf1
kizaunayui <112533480+kizaunayui@users.noreply.github.com>
2026-05-18 21:08:01 +0800

    Fix article structured data URL

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -109,7 +109,7 @@ export const Blog = defineDocumentType(() => ({
         dateModified: doc.lastmod || doc.date,
         description: doc.summary,
         image: doc.images ? doc.images[0] : siteMetadata.socialBanner,
-        url: `${siteMetadata.siteUrl}/${doc._raw.flattenedPath}`,
+        url: `${siteMetadata.siteUrl}/articles/${doc.slug}`,
       }),
     },
   },
