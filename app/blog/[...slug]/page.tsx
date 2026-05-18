da5f4d8c7e065d417606b745cd9527c398a9a313
kizaunayui <112533480+kizaunayui@users.noreply.github.com>
2026-05-18 20:09:03 +0800

    Add static params to old blog redirect

--- app/blog/[...slug]/page.tsx
+++ app/blog/[...slug]/page.tsx
@@ -1,4 +1,9 @@
+import { allBlogs } from 'contentlayer/generated'
 import { redirect } from 'next/navigation'
+
+export const generateStaticParams = async () => {
+  return allBlogs.map((post) => ({ slug: post.slug.split('/').map((name) => decodeURI(name)) }))
+}
 
 export default async function Page(props: { params: Promise<{ slug: string[] }> }) {
   const params = await props.params
