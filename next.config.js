19ee0192f269729211c079811b01f640b616daf8
kizaunayui <112533480+kizaunayui@users.noreply.github.com>
2026-05-18 20:19:26 +0800

    Remove article route redirects

--- next.config.js
+++ next.config.js
@@ -17,37 +17,30 @@ const ContentSecurityPolicy = `
 `
 
 const securityHeaders = [
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
   {
     key: 'Content-Security-Policy',
     value: ContentSecurityPolicy.replace(/\n/g, ''),
   },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
   {
     key: 'Referrer-Policy',
     value: 'strict-origin-when-cross-origin',
   },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
   {
     key: 'X-Frame-Options',
     value: 'DENY',
   },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
   {
     key: 'X-Content-Type-Options',
     value: 'nosniff',
   },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
   {
     key: 'X-DNS-Prefetch-Control',
     value: 'on',
   },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
   {
     key: 'Strict-Transport-Security',
     value: 'max-age=31536000; includeSubDomains',
   },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
   {
     key: 'Permissions-Policy',
     value: 'camera=(), microphone=(), geolocation=()',
@@ -89,25 +82,6 @@ module.exports = () => {
         },
       ],
       unoptimized,
-    },
-    async redirects() {
-      return [
-        {
-          source: '/blog',
-          destination: '/articles',
-          permanent: true,
-        },
-        {
-          source: '/blog/:slug*',
-          destination: '/articles/:slug*',
-          permanent: true,
-        },
-        {
-          source: '/prompts',
-          destination: '/content/prompts',
-          permanent: true,
-        },
-      ]
     },
     async headers() {
       return [
