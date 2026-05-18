25f6cd7cb0f7b54424eebc11ee9a817950f6a78b
kizaunayui <112533480+kizaunayui@users.noreply.github.com>
2026-05-18 20:20:18 +0800

    Restore blog pagination page

--- app/blog/page/[page]/page.tsx
+++ app/blog/page/[page]/page.tsx
@@ -1,14 +1,41 @@
+import ListLayout from '@/layouts/ListLayoutWithTags'
+import { allCoreContent, sortPosts } from 'pliny/utils/contentlayer'
 import { allBlogs } from 'contentlayer/generated'
-import { redirect } from 'next/navigation'
+import { notFound } from 'next/navigation'
 
 const POSTS_PER_PAGE = 5
 
 export const generateStaticParams = async () => {
   const totalPages = Math.ceil(allBlogs.length / POSTS_PER_PAGE)
-  return Array.from({ length: totalPages }, (_, index) => ({ page: (index + 1).toString() }))
+  const paths = Array.from({ length: totalPages }, (_, i) => ({ page: (i + 1).toString() }))
+
+  return paths
 }
 
 export default async function Page(props: { params: Promise<{ page: string }> }) {
   const params = await props.params
-  redirect(`/articles/page/${params.page}`)
+  const posts = allCoreContent(sortPosts(allBlogs))
+  const pageNumber = parseInt(params.page as string)
+  const totalPages = Math.ceil(posts.length / POSTS_PER_PAGE)
+
+  if (pageNumber <= 0 || pageNumber > totalPages || isNaN(pageNumber)) {
+    return notFound()
+  }
+  const initialDisplayPosts = posts.slice(
+    POSTS_PER_PAGE * (pageNumber - 1),
+    POSTS_PER_PAGE * pageNumber
+  )
+  const pagination = {
+    currentPage: pageNumber,
+    totalPages: totalPages,
+  }
+
+  return (
+    <ListLayout
+      posts={posts}
+      initialDisplayPosts={initialDisplayPosts}
+      pagination={pagination}
+      title="All Posts"
+    />
+  )
 }
