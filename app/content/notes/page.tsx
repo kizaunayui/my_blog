018b6f24cb5bca8a37acee3cab4b8ea97d9ba1c7
kizaunayui <112533480+kizaunayui@users.noreply.github.com>
2026-05-26 20:54:25 +0800

    Remove course notes route

--- app/content/notes/page.tsx
+++
@@ -1,9 +0,0 @@
-import { genPageMetadata } from 'app/seo'
-import NotesLibrary from '@/components/NotesLibrary'
-import { notesData } from '@/data/notesData'
-
-export const metadata = genPageMetadata({ title: '课程复习笔记' })
-
-export default function NotesContentPage() {
-  return <NotesLibrary notes={notesData} />
-}
