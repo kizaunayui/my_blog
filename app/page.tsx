e6bb44e996dbd6457d8a14e13c80af4eedcc2717
kizaunayui <112533480+kizaunayui@users.noreply.github.com>
2026-06-02 23:54:21 +0800

    Add MusicPlayer to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { sortPosts } from 'pliny/utils/contentlayer'
 import { allBlogs } from 'contentlayer/generated'
 import Main from './Main'
+import MusicPlayer from '@/components/MusicPlayer'
 
 const POSTS_PER_PAGE = 5
 
@@ -13,5 +14,10 @@ export default async function Page() {
     totalPages,
   }
 
-  return <Main posts={posts} initialDisplayPosts={initialDisplayPosts} pagination={pagination} />
+  return (
+    <>
+      <Main posts={posts} initialDisplayPosts={initialDisplayPosts} pagination={pagination} />
+      <MusicPlayer />
+    </>
+  )
 }
