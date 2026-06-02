8a5c8efc1b9c56b58f7d61241db7142a2aaaad43
kizaunayui <112533480+kizaunayui@users.noreply.github.com>
2026-06-03 01:29:07 +0800

    Allow external audio sources in CSP

--- next.config.js
+++ next.config.js
@@ -7,10 +7,10 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is cdn.jsdelivr.net;
-  style-src 'self' 'unsafe-inline' cdn.jsdelivr.net;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is;
+  style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
-  media-src *.s3.amazonaws.com;
+  media-src 'self' https: blob: data:;
   connect-src *;
   font-src 'self';
   frame-src 'self' giscus.app
