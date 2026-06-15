399bb8cd6584ffbea3877757b726e8a4bdde4b67
kizaunayui <112533480+kizaunayui@users.noreply.github.com>
2026-06-15 18:29:02 +0800

    Place document links inside head

--- app/layout.tsx
+++ app/layout.tsx
@@ -72,26 +72,28 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       className={`${space_grotesk.variable} scroll-smooth`}
       suppressHydrationWarning
     >
-      <link
-        rel="apple-touch-icon"
-        sizes="76x76"
-        href={`${basePath}/static/images/kieran-icon.jpg`}
-      />
-      <link
-        rel="icon"
-        type="image/jpeg"
-        href={`${basePath}/static/images/kieran-icon.jpg`}
-      />
-      <link rel="manifest" href={`${basePath}/static/favicons/site.webmanifest`} />
-      <link
-        rel="mask-icon"
-        href={`${basePath}/static/favicons/safari-pinned-tab.svg`}
-        color="#5bbad5"
-      />
-      <meta name="msapplication-TileColor" content="#000000" />
-      <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
-      <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
-      <link rel="alternate" type="application/rss+xml" href={`${basePath}/feed.xml`} />
+      <head>
+        <link
+          rel="apple-touch-icon"
+          sizes="76x76"
+          href={`${basePath}/static/images/kieran-icon.jpg`}
+        />
+        <link
+          rel="icon"
+          type="image/jpeg"
+          href={`${basePath}/static/images/kieran-icon.jpg`}
+        />
+        <link rel="manifest" href={`${basePath}/static/favicons/site.webmanifest`} />
+        <link
+          rel="mask-icon"
+          href={`${basePath}/static/favicons/safari-pinned-tab.svg`}
+          color="#5bbad5"
+        />
+        <meta name="msapplication-TileColor" content="#000000" />
+        <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
+        <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
+        <link rel="alternate" type="application/rss+xml" href={`${basePath}/feed.xml`} />
+      </head>
       <body className="overflow-x-hidden bg-transparent text-black antialiased dark:text-white">
         <ThemeProviders>
           <RandomBackground basePath={basePath} />
